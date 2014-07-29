222df241aa3ef98cd97a52d30d01ecf17c6a18ff
John Elliott <johnelliott703@gmail.com>
2014-07-29 11:57:24 -0400

    5 done

--- 5.js
+++ 5.js
@@ -2,17 +2,17 @@
 
 var fs = require('fs');
 var path = require('path');
-var directory = "/Users/johnelliott/Desktop/";		// testing line
-//var directory = process.argv[2];
+
+var directory = process.argv[2];
+var extension = process.argv[3];
+
 var dirList = fs.readdir(directory, callback);
 
 function callback (err, files) {
     if (err) throw err;
-//    console.log(files);
-//    console.log(files.sort(path.extname()));
-    var sortedFiles = files.sort(path.extname());
-    for ( i=1; i<sortedFiles.length; i++ ) {
-	console.log(sortedFiles[i]);
+    for (i=0; i<files.length; i++) {
+	if (path.extname(files[i]) == '.' + extension) {
+	    console.log(files[i]);
+	}
     }
 }
-// right now it's printing, but not sorting. I need work out the sorting 
